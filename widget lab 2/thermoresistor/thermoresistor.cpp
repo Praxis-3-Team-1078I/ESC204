1d80151e1ad95c55f432681196c7e3b1593d8772
bt7878 <brandontosteis@gmail.com>
2022-02-11 09:55:46 -0500

    thermoresistior code for lab 2

---
+++ widget lab 2/thermoresistor/thermoresistor.cpp
@@ -0,0 +1,31 @@
+#include "hardware/adc.h"
+#include "hardware/gpio.h"
+#include "pico/stdlib.h"
+#include <iostream>
+#include <limits>
+
+#define THERMORESISTOR_PIN 26
+#define THERMORESISTOR_ADC 0
+#define ADC_REF 3.3
+
+float adc_to_voltage(uint16_t adc_value)
+{
+    return ADC_REF * static_cast<float>(adc_value) / static_cast<float>(std::numeric_limits<uint16_t>::max());
+}
+
+int main()
+{
+    stdio_init_all();
+    adc_init();
+
+    adc_gpio_init(THERMORESISTOR_PIN);
+    adc_select_input(THERMORESISTOR_ADC);
+
+    while (true)
+    {
+        std::cout << adc_to_voltage(adc_read()) << std::endl;
+        sleep_ms(500);
+    }
+
+    return 0;
+}
