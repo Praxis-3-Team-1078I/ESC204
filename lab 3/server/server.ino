f86eb2bf0f5124d582acc3d69e3594572c017696
bt7878 <brandontosteis@gmail.com>
2022-03-19 17:12:47 -0400

    add motor control

--- lab 3/server/server.ino
+++ lab 3/server/server.ino
@@ -4,10 +4,62 @@
 #include <WiFiNINA.h>
 #include <aWOT.h>
 
+#define M1ENA 2
+#define M1IN1 3
+#define M1IN2 4
+#define M2IN3 5
+#define M2IN4 6
+#define M2ENA 9
+
 WiFiServer server(80);
 Application app;
 
 String curDir = "none";
+
+void handleStateChange(String dir)
+{
+    if (dir == "N")
+    {
+        digitalWrite(M1IN1, HIGH);
+        digitalWrite(M1IN2, LOW);
+        digitalWrite(M2IN3, HIGH);
+        digitalWrite(M2IN4, LOW);
+        analogWrite(M1ENA, 255);
+        analogWrite(M2ENA, 255);
+    }
+    else if (dir == "E")
+    {
+        digitalWrite(M1IN1, HIGH);
+        digitalWrite(M1IN2, LOW);
+        digitalWrite(M2IN3, LOW);
+        digitalWrite(M2IN4, HIGH);
+        analogWrite(M1ENA, 255);
+        analogWrite(M2ENA, 255);
+    }
+    else if (dir == "S")
+    {
+        digitalWrite(M1IN1, LOW);
+        digitalWrite(M1IN2, HIGH);
+        digitalWrite(M2IN3, LOW);
+        digitalWrite(M2IN4, HIGH);
+        analogWrite(M1ENA, 255);
+        analogWrite(M2ENA, 255);
+    }
+    else if (dir == "W")
+    {
+        digitalWrite(M1IN1, LOW);
+        digitalWrite(M1IN2, HIGH);
+        digitalWrite(M2IN3, HIGH);
+        digitalWrite(M2IN4, LOW);
+        analogWrite(M1ENA, 255);
+        analogWrite(M2ENA, 255);
+    }
+    else
+    {
+        analogWrite(M1ENA, 0);
+        analogWrite(M2ENA, 0);
+    }
+}
 
 void getState(Request &req, Response &res)
 {
@@ -35,7 +87,12 @@ void setState(Request &req, Response &res)
 
     if (data.hasOwnProperty("curDir"))
     {
-        curDir = (const char *)data["curDir"];
+        String tmp = (const char *)data["curDir"];
+        if (tmp == "none" || tmp == "N" || tmp == "E" || tmp == "S" || tmp == "W")
+        {
+            curDir == tmp;
+            handleStateChange(tmp);
+        }
     }
 
     return getState(req, res);
@@ -43,6 +100,13 @@ void setState(Request &req, Response &res)
 
 void setup()
 {
+    pinMode(M1ENA, OUTPUT);
+    pinMode(M1IN1, OUTPUT);
+    pinMode(M1IN2, OUTPUT);
+    pinMode(M2IN3, OUTPUT);
+    pinMode(M2IN4, OUTPUT);
+    pinMode(M2ENA, OUTPUT);
+
     Serial.begin(115200);
 
     while (!Serial)
