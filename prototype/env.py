17964e460a1814cd492e20bc8513ad19cd2d3f2a
Ted <ted.pinkerton@mail.utoronto.ca>
2022-09-17 14:36:10 -0400

    Final Commit

--- prototype/env.py
+++ prototype/env.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from perlin_numpy import generate_fractal_noise_2d
 
+from PIL import Image
+
 def scale(A, lower=0, upper=1):
     """Scale an np array between lower & upper"""
     return (A-np.min(A))/(np.max(A) - np.min(A))*(upper-lower)+lower
@@ -34,18 +36,27 @@ class Enviroment:
 
 
 if __name__ == '__main__':
-    env = Enviroment((20, 20))
-    cloud1 = env.quality()
-    env.step(100)
-    cloud2 = env.quality()
-    env.step(100)
-    cloud3 = env.quality()
-    env.step(100)
-    cloud4 = env.quality()
+    env = Enviroment((256, 256))
 
-    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
-    ax1.imshow(cloud1, cmap="gray", interpolation='lanczos')
-    ax2.imshow(cloud2, cmap="gray", interpolation='lanczos')
-    ax3.imshow(cloud3, cmap="gray", interpolation='lanczos')
-    ax4.imshow(cloud4, cmap="gray", interpolation='lanczos')
-    plt.show()
+    rain = []
+    for _ in range(14):
+        env.step(100)
+        rain.append(env.quality())
+
+    real = sum(rain)/len(rain)
+    noisy = real+np.random.normal(0, 1, size=real.shape)
+
+    real = scale(real, upper=255)
+    noisy = scale(noisy, upper=255)
+    fixed = (real+noisy)/2
+
+    print(np.mean(np.abs(real-noisy)/255))
+    print(np.mean(np.abs(real-fixed)/255))
+
+    real = Image.fromarray(real).convert("RGB")
+    noisy = Image.fromarray(noisy).convert("RGB")
+    fixed = Image.fromarray(fixed).convert("RGB")
+
+    real.save("real.png")
+    noisy.save("noisy.png")
+    fixed.save("fixed.png")
